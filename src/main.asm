3926c33515e7d97df51991d1862b56d5f00ce461
Sourabh Singh <specious0027@gmail.com>
2025-09-01 11:49:49 +0530

    updated

--- src/main.asm
+++ src/main.asm
@@ -1,43 +1,40 @@
-; BIOS == Basic Input Output System
-
-org 0x7C00
+; BIOS = Basic Input Output System
+org 0x7c00
 bits 16
 
-%define ENDL 0x0D,0x0A
-msg_hello: db 'Hello World!', 0
-;db = declare byte
-
+mesg_hello db 'Hello, World!',0
+;db = define byte
 main:
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
+    mov ax,0
+    mov ds,ax
+    mov es,ax
 
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov si, msg_hello
+    mov ss,ax
+    mov sp,0x7c00
+    mov si,mesg_hello
     call puts
     hlt
+
 puts:
     push si
     push ax
 .loop:
-    lodsb  ;load next char in al
-    cmp al, 0
+    lodsb
+    cmp al,0
     je .done
-    mov ah, 0x0E
-    mov bh, 0
+    mov ah,0x0e
+    mov bh,0
     int 0x10
     jmp .loop
 
+
 .done:
     pop si
-    pop ax
+    pop ax        ;THe repition was happening case i poped ax before si
     ret
 
-.hatl:
-    jmp .hatl
-
+.halt:
+    jmp .halt
 times 510-($-$$) db 0
-dw 0AA55h
-;dw = declare word (2 bytes)
+dw 0xaa55
+;dw = define word
